c5b929617b4ba0f61ba1de725a2b602a0daa2b0f
div <rasaniadivya1@gmail.com>
2026-08-15 23:22:55 -0400

    made mobile responsive

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mouseGlow = document.getElementById("mouseGlow");
 
-document.addEventListener("mousemove", (e) => {
-  mouseGlow.style.left = e.clientX - 150 + "px";
-  mouseGlow.style.top = e.clientY - 150 + "px";
-});
+const canHover = window.matchMedia("(hover: hover) and (pointer: fine)").matches;
+
+if (mouseGlow && canHover) {
+  document.addEventListener("mousemove", (e) => {
+    mouseGlow.style.left = e.clientX - 150 + "px";
+    mouseGlow.style.top = e.clientY - 150 + "px";
+  });
+}
